4ff3313683e763d1ef381b6819477688035516e5
Kevin Barabash <kevinb@khanacademy.org>
2017-05-18 23:02:54 -0400

    print out various properties of mouse strokes on the canvas

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const drags = downs.flatMap((down) => {
 const nextDrags = drags.skip(1);
 const dragPairs = Kefir.zip([drags, nextDrags]);
 
-const thickness = 10;
+const thickness = 2;
 const radius = thickness / 2;
 
 // dragPairs.observe((pair) => console.log(pair));
@@ -129,12 +129,163 @@ const calcBounds = (points) => points.reduce((accum, p) => {
     yMax: -Infinity,
 });
 
+const dot = (u, v) => u.x * v.x + u.y * v.y;
 
-strokes.onValue((stroke) => {
-    const angles = calcAngles(stroke);
-    const anglesNoDiscont = removeDiscontinuities(angles);
+const mag = (v) => Math.sqrt(dot(v, v));
 
-    const bounds = calcBounds(stroke);
+const calcCurvature = (points) => {
+    const results = [];
+
+    for (let i = 0; i < points.length - 2; i++) {
+        const a = points[i+0];
+        const b = points[i+1];
+        const c = points[i+2];
+
+        const A = 0.5 * ((a.x - c.x) * (b.y - a.y) - (a.x - b.x) * (c.y - a.y));
+        const K = 4 * A / (distance(a, b) * distance(b, c) * distance(c, a));
+
+        if (K !== 0) {
+            console.log(1 / K);
+        }
+
+        results.push(K);
+    }
+
+    return results;
+}
+
+
+const distance = (p1, p2) => {
+    const dx = p2.x - p1.x;
+    const dy = p2.y - p1.y;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+
+const mapPairs = (values, callback) => {
+    const results = [];
+    for (let i = 0; i < values.length - 1; i++) {
+        results.push(callback(values[i], values[i + 1]));
+    }
+    return results;
+}
+
+const reducePairs = (values, callback, init) => {
+    let result = init;
+    for (let i = 0; i < values.length - 1; i++) {
+        result = callback(result, values[i], values[i + 1]);
+    }
+    return result;
+}
+
+// TODO(kevinb) rename to arcLength?
+const strokeLength = (points) =>
+    reducePairs(points, (result, p1, p2) => result + distance(p1, p2), 0);
+
+const calcDeltas = (scalars) => mapPairs(scalars, (k1, k2) => k2 - k1);
+
+const add = (a, b) => a + b;
+
+const sum = (xs) => xs.reduce(add);
+
+const mean = (xs) => sum(xs) / xs.length;
+
+const square = (a) => a * a;
+
+const variance = (xs) => {
+    const mu = mean(xs);
+    return sum(xs.map(x => square(x - mu))) / xs.length;
+}
+
+const stddev = (xs) => Math.sqrt(variance(xs));
+
+// A queue of strokes that are currently being evaluated
+const recognizing = [];
+
+// Each stroke should be giving a set of features/properties and the confidence in those features
+// e.g.
+// straight line
+// - angle (implies direction as well)
+// vertical (this can be determined from angle)
+// - ascending/descending
+// horizontal
+// - left/right
+// ...
+
+// Detecting straight lines
+// weight average of slopes
+// calc stddev... if any point is a certain distance from the line segment than it's no longer straight
+
+const possibleGlyphs = [];
+
+// Strokes or groups of strokes that have been recognized as glyphs
+const recognized = [];
+
+
+strokes.onValue((points) => {
+    const bounds = calcBounds(points);
+
+    const w = bounds.xMax - bounds.xMin;
+    const h = bounds.yMax - bounds.yMin;
+
+    if (w > h) {
+        console.log('horizontal');
+        console.log(`h / w = ${h / w}`);
+
+        const ratio = h / w;
+
+        // each feature has some sort of probabbility
+        // the range is from 0 to 0.5
+        // between 0 and 0.05 it's 100%
+        // after 0.25 it's 100% not
+        let prob = 1.0;
+        if (ratio > 0.25) {
+            prob = 0.0;
+        } else if (ratio > 0.05) {
+            prob = 1.0 - (ratio - 0.05) / 0.2;
+        }
+
+        console.log(`prob = ${prob}`);
+    } else {
+        console.log('vertical');
+        console.log(`w / h = ${w / h}`);
+
+        const ratio = w / h;
+
+        // each feature has some sort of probabbility
+        // the range is from 0 to 0.5
+        // between 0 and 0.05 it's 100%
+        // after 0.25 it's 100% not
+        let prob = 1.0;
+        if (ratio > 0.25) {
+            prob = 0.0;
+        } else if (ratio > 0.05) {
+            prob = 1.0 - (ratio - 0.05) / 0.2;
+        }
+
+        console.log(`prob = ${prob}`);
+    }
+
+
+    // const ks = calcCurvature(points);
+    // console.log('curvatures: %o', ks);
+
+    // const length = strokeLength(points);
+    // console.log(`strokeLength = ${length}`);
+
+    const angles = calcAngles(points);
+    const anglesNoDiscont = removeDiscontinuities(angles).map((angle) => angle.toFixed(1));
+
+    // console.log(anglesNoDiscont);
+
+    const angleDeltas = calcDeltas(angles);
+    // console.log('angleDeltas: %o', angleDeltas);
+
+    // console.log(`mean = ${mean(angleDeltas)}`);
+    // console.log(`variance = ${variance(angleDeltas)}`);
+    // console.log(`stddev = ${stddev(angleDeltas)}`);
+    // console.log('');
+
     ctx.strokeStyle = 'red';
     ctx.lineWidth = 1;
 
@@ -154,7 +305,7 @@ strokes.onValue((stroke) => {
     // TODO get the distance covered by the dissenting direction and compare
     // that aginst the distance covered by the majority direction
     const directions = statDirection(anglesNoDiscont);
-    console.log(directions);
+    // console.log('directions: %o', directions);
 
     // TODO get the magnitude of the direction changes... if there aren't any
     // big changes then it's probably a straight line
@@ -163,7 +314,7 @@ strokes.onValue((stroke) => {
 
     // A bimodal distribution indicates some sort of angular stroke
 
-    console.log('%o', anglesNoDiscont);
+    // console.log('anglesNoDiscont: %o', anglesNoDiscont);
 
 
     // start with detect staight lines
